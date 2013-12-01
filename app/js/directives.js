be857f20b00ed45e41d1481043b6219d640e122c
freakru <o.giese@gmail.com>
2013-12-01 20:10:23 +0100

    fixed autoclode for datepicker

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,7 +5,8 @@ hbApp.directive('bDatepicker', function() {
         require: 'ngModel',
         link: function(scope, el, attr, ngModel) {
             el.datepicker({
-                weekStart: 1
+                weekStart: 1,
+                autoclose: true
             }).on('changeDate', function(e) {
                 scope.$apply(function() {
                     var date = moment(e.date).format('DD.MM.YYYY');
