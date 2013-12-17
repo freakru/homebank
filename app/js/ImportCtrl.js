89bdc684c15b26ec90b1091f1f01c4ed6b0d1c2e
freakru <o.giese@gmail.com>
2013-12-17 20:44:21 +0100

    removed comments from import controller

--- app/js/ImportCtrl.js
+++ app/js/ImportCtrl.js
@@ -1,23 +1,15 @@
 hbControllers.controller('ImportCtrl', ['$scope', '$upload',
     function($scope, $upload) {
         $scope.onFileSelect = function($files) {
-            //$files: an array of files selected, each file has name, size, and type.
             for (var i = 0; i < $files.length; i++) {
                 var $file = $files[i];
                 $scope.upload = $upload.upload({
-                    url: '/homebank/index.php/api/transaction/import', //upload.php script, node.js route, or servlet url
-                    // method: POST or PUT,
-                    // headers: {'headerKey': 'headerValue'}, withCredential: true,
+                    url: '/homebank/index.php/api/transaction/import',
                     data: {myObj: $scope.myModelObj},
                     file: $file,
-                    /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-                    //fileFormDataName: myFile,
-                    /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
-                    //formDataAppender: function(formData, key, val){} 
                 }).progress(function(evt) {
                     console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
                 }).success(function(data, status, headers, config) {
-                    // file is uploaded successfully
                     console.log(data);
                 });
                 //.error(...)
